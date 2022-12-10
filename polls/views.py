57de405eecbc7f66bf0cf02a6ab9aa466a0825b6
Alex Poklonsky <jispar@mail.ru>
2022-12-10 16:25:17 +0300

    Add cve.

--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,9 @@
 from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    latest_question_list = ["hello", "what??!?"]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context={
+            'latest_question_list': ["hello", "what??!?", "<script>alert('XSS');</script>"],
+        })
