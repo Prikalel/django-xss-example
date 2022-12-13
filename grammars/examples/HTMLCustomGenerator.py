236eef67137bea37bbf50527a601bfc84804345a
Alex Poklonsky <jispar@mail.ru>
2022-12-13 11:07:46 +0300

    Add example from grammarinator.

---
+++ grammars/examples/HTMLCustomGenerator.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2017-2021 Renata Hodovan, Akos Kiss.
+#
+# Licensed under the BSD 3-Clause License
+# <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
+# This file may not be copied, modified, or distributed except
+# according to those terms.
+
+import json
+import random
+
+from os.path import dirname, join
+
+from grammarinator.runtime import *
+
+from HTMLGenerator import HTMLGenerator
+
+
+with open(join(dirname(__file__), 'html.json')) as f:
+    tags = json.load(f)
+
+tag_names = list(tags.keys())
+
+
+class HTMLCustomGenerator(HTMLGenerator):
+
+    attr_stack = []
+    tag_stack = []
+    tags = set()
+
+    # Customize the function generated from the htmlTagName parser rule to produce valid tag names.
+    def htmlTagName(self, parent=None):
+        current = UnparserRule(name='htmlTagName', parent=parent)
+        name = random.choice(tags[self.tag_stack[-1]]['children'] or tag_names if self.tag_stack else tag_names)
+        self.tag_stack.append(name)
+        UnlexerRule(src=name, parent=current)
+        self.tag_stack.append(name)
+        return current
+
+    # Customize the function generated from the htmlAttributeName parser rule to produce valid attribute names.
+    def htmlAttributeName(self, parent=None):
+        current = UnparserRule(name='htmlAttributeName', parent=parent)
+        name = random.choice(list(tags[self.tag_stack[-1]]['attributes'].keys()) or ['""'])
+        self.attr_stack.append(name)
+        UnlexerRule(src=name, parent=current)
+        return current
+
+    # Customize the function generated from the htmlAttributeValue parser rule to produce valid attribute values
+    # to the current tag and attribute name.
+    def htmlAttributeValue(self, parent=None):
+        current = UnparserRule(name='htmlAttributeValue', parent=parent)
+        UnlexerRule(src=random.choice(tags[self.tag_stack[-1]]['attributes'].get(self.attr_stack.pop(), ['""']) or ['""']), parent=current)
+        return current
+
+    def _endOfHtmlElement(self):
+        self.tag_stack.pop()
+
+    # You probably want to rewrite this with a distinct CSS fuzzer.
+    def style_sheet(self, parent=None):
+        return UnlexerRule(src='* { background: green; }', parent=parent)
